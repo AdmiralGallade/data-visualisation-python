baf33d0b564ad8c449387c1ee818abb51566869f
AdmiralGallade <mailforkushagra2@gmail.com>
2020-05-30 00:25:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ df = pd.read_csv('temporal.csv')
 #print(df.describe())
 
 
+format_dict = {'data science':'${0:,.2f}', 'Mes':'{:%m-%Y}', 'machine learning':'{:.2%}'}
+#We make sure that the Month column has datetime format
+df['Mes'] = pd.to_datetime(df['Mes'])
+#We apply the style to the visualization
+print(df.head().style.format(format_dict))
