1ac02692d5949254f416b959e37b4e2078fee1d2
AdmiralGallade <mailforkushagra2@gmail.com>
2020-05-30 00:35:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pandas as pd
-df = pd.read_csv('temporal.csv')
+import matplotlib.pyplot as plt
 
 
 #print(df.head(10)) #View first 10 data rows
 
 #print(df.describe())
 
-format_dict = {'Mes':'{:%m-%Y}'} #Simplified format dictionary with values that do make sense for our data
-df.head().style.format(format_dict).highlight_max(color='darkgreen').highlight_min(color='#ff0000')
+# format_dict = {'Mes':'{:%m-%Y}'} #Simplified format dictionary with values that do make sense for our data
+# df.head().style.format(format_dict).highlight_max(color='darkgreen').highlight_min(color='#ff0000')
+
+#using python interactive.
+
+df = pd.read_csv('temporal.csv')
+
+plt.plot(df['Mes'], df['data science'], label='data science') #The parameter label is to indicate the legend. This doesn't mean that it will be shown, we'll have to use another command that I'll explain later.
+
+
