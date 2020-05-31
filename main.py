60d3924e26890f5106d395f980962ea9e65d891d
AdmiralGallade <mailforkushagra2@gmail.com>
2020-05-31 01:03:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -123,5 +123,13 @@ m2.save('map2.html')
 
 
 
+#geopanda
+
+from geopandas.tools import geocode
+df2 = pd.read_csv('mapa.csv')
+df2.dropna(axis=0, inplace=True)
+df2['geometry'] = geocode(df2['País'], provider='nominatim')['geometry'] #It may take a while because it downloads a lot of data.
+df2['Latitude'] = df2['geometry'].apply(lambda l: l.y)
+df2['Longitude'] = df2['geometry'].apply(lambda l: l.x)
 
 
